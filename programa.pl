b5bfb5766281705370e0efb68dffab2c9fa17c98
Reichel-eze <reicheleze@gmail.com>
2024-08-08 14:48:00 -0300

    Realizado el 4) pero no el 5)

--- programa.pl
+++ programa.pl
@@ -63,3 +63,63 @@ estaEnDosRedes(Usuario) :-
     canal(Usuario, Red2, _),
     Red1 \= Red2.
 
+% 3) Tipos de Contenidos
+% video(QuienesAparecen, Duracion)
+% foto(QuienesAparecen)
+% stream(Tematica)
+
+% a) Modelar los contenidos de forma tal que a futuro pueden existir 
+% otros tipos de contenido 
+
+publico(ana, tiktok, video([beto, evelyn], 1)).
+publico(ana, tiktok, video([ana], 1)).
+publico(ana, instagram, foto([ana])).
+
+publico(beto, instagram, foto([])).
+
+publico(cami, twitch, stream(leagueOfLegends)).
+publico(cami, youtube, video([cami], 5)).
+
+publico(evelyn, instagram, foto([evelyn, cami])).
+
+% b) Se sabe que las temáticas relacionadas con juegos 
+% son leagueOfLegends, minecraft y aoe. Agregar esta 
+% información a la base de conocimientos.
+
+juego(leagueOfLegends).
+juego(minecraft).
+juego(aoe).
+
+% 4) adictiva/1 se cumple para una red cuando sólo tiene contenidos
+% adictivos (Un contenido adictivo es un video de menos de 3
+% minutos, un stream sobre una temática relacionada con juegos, 
+% o una foto con menos de 4 participantes)
+
+adictiva(Red) :-
+    red(Red),
+    forall(publico(_,Red,Contenido), contenidoAdictivo(Contenido)).
+
+contenidoAdictivo(video(_,Duracion)) :- Duracion < 3.
+contenidoAdictivo(stream(Tema)) :- juego(Tema).
+contenidoAdictivo(foto(Participantes)) :-
+    length(Participantes, Cantidad),
+    Cantidad < 4.
+
+% 5) colaboran/2 se cumple cuando un usuario aparece en las redes 
+% de otro (en alguno de sus contenidos). En un stream siempre 
+% aparece quien creó el contenido.
+% Esta relación debe ser simétrica. (O sea, si a colaboró con b, 
+% entonces también debe ser cierto que b colaboró con a)
+% Por ejemplo, beto colaboró con ana y ana colaboró con evelyn.
+
+colaboran(Usuario, OtroUsuario) :-
+    publicoContenidoCon(Usuario, OtroUsuario).
+
+colaboran(Usuario, OtroUsuario) :-
+    publicoContenidoCon(Usuario, OtroUsuario).
+    
+publicoContenidoCon(Usuario, OtroUsuario) :-
+    publico(Usuario, _, Contenido),
+    apareceEn(Usuario, Contenido, OtroUsuario).
+    
+
